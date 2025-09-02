7180cbba67d0fad0fea578429ddab89114386d1c
Gawc1uuu <gawlikjakub9@gmail.com>
2025-09-02 21:02:15 +0200

    format: add prettier and husky

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-red-400 text-8xl">Hello World</h1>
+      <h1 className="text-red-400 text-8xl">Hello
+        
+         World</h1>
     </div>
   );
 }
