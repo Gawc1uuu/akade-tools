908c1d3a8ee2a8b151b4d095b512508ee74f3fdf
Gawc1uuu <gawlikjakub9@gmail.com>
2025-09-02 21:02:35 +0200

    testing husky

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-red-400 text-8xl">Hello
-        
-         World</h1>
+      <h1 className="text-red-400 text-8xl">Hello World</h1>
     </div>
   );
 }
