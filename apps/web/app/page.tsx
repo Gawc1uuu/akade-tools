3418a5ca018c800d5b594485a2cb43883ce4789c
Gawc1uuu <gawlikjakub9@gmail.com>
2025-09-09 23:05:38 +0200

    setting up shadcn

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from '@repo/ui/button';
+import { Button } from "~/components/ui/button";
 
 export default async function Home() {
   return (
     <div>
       <h1 className="text-red-400 text-8xl">Hello World</h1>
-      <Button>elo</Button>
+      <Button>click me</Button>
     </div>
   );
 }
