cdb7f544e3c311512b26f92b6b136c22b4faef78
Gawc1uuu <gawlikjakub9@gmail.com>
2025-09-09 21:49:59 +0200

    adding relations to schema

--- packages/db/src/schema/schema.ts
+++ packages/db/src/schema/schema.ts
@@ -1,10 +1,11 @@
+import { relations } from 'drizzle-orm';
 import { pgEnum } from 'drizzle-orm/pg-core';
 import { pgTable, text, timestamp, varchar } from 'drizzle-orm/pg-core';
 import { ulid } from 'ulid';
 
 export const userStatusEnum = pgEnum('user_status', ['INVITED', 'ACTIVE', 'BLOCKED']);
 
-export const UsersTable = pgTable('users', {
+export const users = pgTable('users', {
   id: varchar('id').primaryKey().$defaultFn(ulid),
   email: varchar('email').notNull().unique(),
   password: varchar('password'),
@@ -12,7 +13,7 @@ export const UsersTable = pgTable('users', {
   createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
 });
 
-export const CarsTable = pgTable('cars', {
+export const cars = pgTable('cars', {
   id: varchar('id').primaryKey().$defaultFn(ulid),
   make: varchar('make').notNull(),
   model: varchar('model').notNull(),
@@ -20,4 +21,16 @@ export const CarsTable = pgTable('cars', {
   inspectionEndDate: timestamp('inspection_end_date', { withTimezone: true }),
   createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
   updatedAt: timestamp('updated_at', { withTimezone: true }).notNull().defaultNow(),
+  userId:varchar("user_id").notNull()
 });
+
+export const usersRelations = relations(users,({many})=>({
+  cars:many(cars)
+}))
+
+export const carsRelations = relations(cars, ({one})=>({
+  owner:one(users,{
+    fields:[cars.userId],
+    references:[users.id]
+  })
+}))
