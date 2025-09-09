1f243d77ced59570a063fb8d31b2723f816489d8
Gawc1uuu <gawlikjakub9@gmail.com>
2025-09-09 21:44:54 +0200

    db schema formatting

--- packages/db/src/schema/schema.ts
+++ packages/db/src/schema/schema.ts
@@ -2,22 +2,22 @@ import { pgEnum } from 'drizzle-orm/pg-core';
 import { pgTable, text, timestamp, varchar } from 'drizzle-orm/pg-core';
 import { ulid } from 'ulid';
 
-export const userStatusEnum = pgEnum("user_status",["INVITED","ACTIVE","BLOCKED"])
+export const userStatusEnum = pgEnum('user_status', ['INVITED', 'ACTIVE', 'BLOCKED']);
 
 export const UsersTable = pgTable('users', {
   id: varchar('id').primaryKey().$defaultFn(ulid),
   email: varchar('email').notNull().unique(),
   password: varchar('password'),
-  status:userStatusEnum("status").default("INVITED").notNull(),
-  createdAt: timestamp('created_at',{withTimezone:true}).notNull().defaultNow(),
+  status: userStatusEnum('status').default('INVITED').notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
 });
 
-export const CarsTable = pgTable("cars",{
-  id:varchar("id").primaryKey().$defaultFn(ulid),
-  make:varchar("make").notNull(),
-  model:varchar("model").notNull(),
-  insuranceEndDate:timestamp("insurance_end_date",{withTimezone:true}),
-  inspectionEndDate:timestamp("inspection_end_date",{withTimezone:true}),
-  createdAt:timestamp("created_at",{withTimezone:true}).notNull().defaultNow(),
-  updatedAt:timestamp("updated_at",{withTimezone:true}).notNull().defaultNow()
-})
+export const CarsTable = pgTable('cars', {
+  id: varchar('id').primaryKey().$defaultFn(ulid),
+  make: varchar('make').notNull(),
+  model: varchar('model').notNull(),
+  insuranceEndDate: timestamp('insurance_end_date', { withTimezone: true }),
+  inspectionEndDate: timestamp('inspection_end_date', { withTimezone: true }),
+  createdAt: timestamp('created_at', { withTimezone: true }).notNull().defaultNow(),
+  updatedAt: timestamp('updated_at', { withTimezone: true }).notNull().defaultNow(),
+});
