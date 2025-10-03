d0554b9c6ab02781993f0414a76b290180f02d47
Gawc1uuu <gawlikjakub9@gmail.com>
2025-10-03 15:18:00 +0200

    connect logout to nav user

--- apps/web/components/nav-user.tsx
+++ apps/web/components/nav-user.tsx
@@ -12,6 +12,7 @@ import {
 } from '~/components/ui/dropdown-menu';
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from '~/components/ui/sidebar';
 import { Session } from '~/lib/types';
+import { logout } from '~/app/actions/auth';
 
 export function NavUser({ user }: { user: Session }) {
   const { isMobile, state } = useSidebar();
@@ -53,7 +54,7 @@ export function NavUser({ user }: { user: Session }) {
               </div>
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuItem className="hover:bg-gray-500/50 hover:cursor-pointer focus:bg-gray-500/50 focus:text-white [&_svg]:text-white">
+            <DropdownMenuItem onSelect={() => logout()} className="hover:bg-gray-500/50 hover:cursor-pointer focus:bg-gray-500/50 focus:text-white [&_svg]:text-white">
               <LogOut className="text-white" />
               Wyloguj się
             </DropdownMenuItem>
